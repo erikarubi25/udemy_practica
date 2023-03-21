df232553448736ebe15cfba2264764fe7d3e89cc
erikarubi25 <erikarubi.2579@gmail.com>
2023-03-20 18:27:23 -0600

    en contac.php fue un cambio minimo de revision y en design.css fue el formulario material design

--- contact.php
+++ contact.php
@@ -7,9 +7,9 @@ $mailheader = "From: ".$_POST["email"]."\r\n";
 $mailheader .= "Reply: ".$_POST["email"]."\r\n";
 $mailheader .= "Content-type: text/html; charset=utf-8\r\n";
 
-$MESSAGE_BODY = "Nombre: ".$_POST["name"]."\r\n";
-$MESSAGE_BODY .= "\n<br>Email: ".$_POST["email"]."\r\n";
-$MESSAGE_BODY .= "\n<br>Mensaje: ".$_POST["message"]."\r\n";
+$MESSAGE_BODY = "Nombre: ".$_POST["name"]."\n";
+$MESSAGE_BODY .= "\n<br>Email: ".$_POST["email"]."\n";
+$MESSAGE_BODY .= "\n<br>Mensaje: ".nl2br($_POST["message"])."\n";
 
 mail($para, $asunto, $MESSAGE_BODY, $mailheader) or die ("Error al enviar email");
 
